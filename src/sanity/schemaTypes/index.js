84cd7eaea8c6291acac08a36fc6c31dfa12e86d1
Vera <althinivera77@gmail.com>
2025-04-25 16:14:01 +0200

    Removed some schemas that will not be used in the project

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,9 +1,7 @@
 import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
 import { eventType } from './eventType'
 
 export const schema = {
-  types: [blockContentType, categoryType, postType, authorType, eventType],
+  types: [blockContentType, categoryType, eventType],
 }
