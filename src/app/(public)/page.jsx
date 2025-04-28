ec5dbbc1c09221d1f62353a7670bacd05bddfeb7
Vera <althinivera77@gmail.com>
2025-04-28 17:40:28 +0200

    Fetched events and displayed them on the home page (including slug links to each event)

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -1,8 +1,13 @@
+import { EventsSection } from "@/components/eventsSection"
+
 function HomePage() {
   return (
     <div>
       <h1>Welcome to Event Finder!</h1>
       <h2>Browse and create all kinds of fun events for everyone!</h2>
+      <div>
+        <EventsSection />
+      </div>
     </div>
   )
 }
--- src/components/eventsSection.jsx
+++ src/components/eventsSection.jsx
@@ -1,7 +1,33 @@
-export const EventsSection = () => {
+import { getAllEvents } from "@/sanity/lib/api"
+import Image from "next/image"
+import Link from "next/link"
+
+export const EventsSection = async() => {
+
+  const events = await getAllEvents()
+  console.log(events)
+
   return (
     <div>
       <h2>Events:</h2>
+      <div className="grid grid-cols-3 gap-5">
+          {
+            events && events.map(event => (
+              <Link key={event._id} href={`/events/${event.slug}`}>
+                  <div>
+                      <Image 
+                      src={event.image.url} 
+                      alt={event.image.alt} 
+                      width={300} 
+                      height={300}
+                      className="w-full h-full object-cover"
+                      />
+                  </div>
+                  <h3 className="text-center">{event.title}</h3>
+              </Link>
+            ))
+          }
+      </div>
     </div>
   )
 }
